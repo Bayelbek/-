75df99ef60bac5deadec536ebf836a7ac39cec46
Bayel <bayel.durusbekov.kk@gmail.com>
2021-12-30 11:15:07 +0600

    master

---
+++ src/master/master.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Master(props) {
+    return (
+        <>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab aliquam ducimus eaque, eligendi eum ipsum itaque labore nesciunt quas reiciendis, tempore temporibus voluptatibus. Eligendi esse fugit nostrum sapiente voluptatibus?
+        </>
+    );
+}
+
+export default Master;
